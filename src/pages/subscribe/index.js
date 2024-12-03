baf1653678a98d8221b0e30e4342bfd6b2d6aca7
Abiha Rasheed <167268631+Codebybiya@users.noreply.github.com>
2024-12-03 16:09:07 +0500

    Update index.js

--- src/pages/subscribe/index.js
+++ src/pages/subscribe/index.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "../components/CheckoutForm";
+import CheckoutForm from "../../components/CheckoutForm";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
